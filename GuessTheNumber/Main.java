b62fa9a83836ea443d7d4077045cfd40aecbe5aa
Ashley Pleasant <95384986+hehnu@users.noreply.github.com>
2022-06-05 08:45:57 -0400

    Update Main.java

--- GuessTheNumber/Main.java
+++ GuessTheNumber/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Hello! What is your name?");
         String hello = String.valueOf(name.next());
         System.out.println("Well " + hello + ", I am thinking of a number between 1 and 20.");
-               // "Are you ready?"); y or n
+               
         GTN.game(hello);
     }
 }
