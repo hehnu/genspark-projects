f11bfc410fdf20aec10ad10f10005bfc62b6e5d6
Ashley <nakatulog3@protonmail.com>
2022-06-13 11:26:43 -0400

    2nd update

---
+++ GuessTheNumber/GTNTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GTNTest {
+    Main main;
+
+    GTN game;
+
+    @BeforeEach
+    void setUp() {
+        game=new GTN();
+
+
+    }
+
+    @Test
+    void game(){
+
+
+    String hello="Ashley";
+    game.guess=10;
+    game.number=10;
+    int i=1;
+
+
+
+        assertEquals(10,10,"Good job, " + hello +
+                "! \n"
+                + "You guessed my number in " + i + " guesses!");
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
