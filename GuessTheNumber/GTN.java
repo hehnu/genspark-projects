aae80bbc529e12f6a79487b386f2415e6aff0532
Ashley <nakatulog3@protonmail.com>
2022-06-13 11:25:05 -0400

    2nd update

--- GuessTheNumber/GTN.java
+++ GuessTheNumber/GTN.java
@@ -1,79 +1,85 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
 public class GTN {
-
+    public int guess;
+    public int number;
 
 
     //public int guess;
     //public int random;
-   // Scanner input = new Scanner(System.in);
+    // Scanner input = new Scanner(System.in);
 
-   // public GTN(String hello, int guess, int i,int number) {
-        // Scanner input = new Scanner(System.in);
-       // int number = 1 + (int) (20 * Math.random());
-       // int A = 6;
-   //public String correctAnswer() {}
-
-
-
-        public static void anotherRound (String hello){
-            Scanner input = new Scanner(System.in);
-            System.out.println("Would you like to play again? (Y or N)");
-            String answer = input.next();
-            if (answer == "Y" || answer == "y") {
-                game(hello);
-
-            } else {
-                System.out.print("Goodbye!");
-                return;
-            }
-        }
+    // public GTN(String hello, int guess, int i,int number) {
+    // Scanner input = new Scanner(System.in);
+    // int number = 1 + (int) (20 * Math.random());
+    // int A = 6;
+    //public String correctAnswer() {}
 
 
-        private static void game(String hello) {
+    public static void anotherRound(String hello) {
+
         Scanner input = new Scanner(System.in);
-          int number = 1 + (int) (20 * Math.random());
-         int A = 6;
-        int i,guess;
+        System.out.println("Would you like to play again? (Y or N)");
+        String answer = String.valueOf(input.next());
+        if (answer.equals("Y") || answer.equals("N")){
+            GTN.game(hello);
+
+        } else {
+            System.out.print("Goodbye!");
+            return;
+        }
+    }
 
 
+    public static void game(String hello) {
+        Scanner input = new Scanner(System.in);
+        int number = 1 + (int) (20 * Math.random());
+        int A = 6;
+        int i, guess;
 
 
-            for (i = 1; i < A; i++) {
-                System.out.println("Take a guess: ");
+        for (i = 1; i <= A; i++) {
+            System.out.println("Take a guess: ");
 
-                try {
-                    guess = input.nextInt();
+            try {
+                guess = input.nextInt();
 
-                    //If guess is correct
-                    if (number == guess) {
-                        System.out.println("Good job, " + hello +
-                                "! \n"
-                                + "You guessed my number in " + i + " guesses!");
-                        anotherRound(hello);
-                        break;
-
-                    } else if (number > guess && i != A - 1) {
-                        System.out.println(hello + ", your guess is too high. Try again.");
-                    } else if (number < guess && i != A - 1) {
-                        System.out.println(hello + ", your guess is too low. Try again.");
-                    }
-
-                } catch (Exception e) {
-
-                    System.out.println("Whaa?");
+                //If guess is correct
+                if (number == guess) {
+                    System.out.println("Good job, " + hello +
+                            "! \n"
+                            + "You guessed my number in " + i + " guesses!");
+                    anotherRound(hello);
                     break;
                 }
                 if (i == A) {
                     System.out.println("Dang, you're out of guesses! \n" +
                             "My secret number is: " + number + "!");
                     anotherRound(hello);
+                    break;
                 }
+
+
+                     else if (number > guess && i != A - 1) {
+                        System.out.println(hello + ", your guess is too high. Try again.");
+                    } else if (number < guess && i != A - 1) {
+                        System.out.println(hello + ", your guess is too low. Try again.");
+                    }
+
+
+
+
+            } catch (Exception e) {
+
+
+                System.out.println("Whaa?");
+                break;
 
             }
         }
 
 
+    }
 }
 
 
--- GuessTheNumber/Main.java
+++ GuessTheNumber/Main.java
@@ -4,8 +4,8 @@ public class Main {
         Scanner name = new Scanner(System.in);
         System.out.println("Hello! What is your name?");
         String hello = String.valueOf(name.next());
-        System.out.println("Well " + hello + ", I am thinking of a number between 1 and 20. Are you ready? (y or n)");
+        System.out.println("Well " + hello + ", I am thinking of a number between 1 and 20.");
         //Scanner ready = new Scanner(System.in);  // "Are you ready?"); y or n
-
+        GTN.game(hello);
     }
 }
